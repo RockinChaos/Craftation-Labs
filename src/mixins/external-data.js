a1cc2c502e9407a8073cd6e135c2d72f3dd8608d
RockinChaos <github@CraftationGaming.com>
2025-09-09 19:46:45 -0700

    fix: caching

--- src/mixins/external-data.js
+++ src/mixins/external-data.js
@@ -76,11 +76,11 @@ const pluginsDefaults = {
 
 const state = Vue.observable({
     github: {
-        stars: JSON.parse(localStorage.getItem('gitStats'))?.stars || 400,
-        forks: JSON.parse(localStorage.getItem('gitStats'))?.forks || 15,
+        stars: JSON.parse(getLocalStorage('gitStats'))?.stars || 400,
+        forks: JSON.parse(getLocalStorage('gitStats'))?.forks || 15,
     },
     discord: {
-        members: JSON.parse(localStorage.getItem('discordMembers'))?.members || 404,
+        members: JSON.parse(getLocalStorage('discordMembers'))?.members || 404,
     },
     static: {
         year: new Date().getFullYear(),
@@ -109,7 +109,7 @@ const state = Vue.observable({
         }
     },
     projects: Object.keys(pluginsDefaults).length,
-    downloads: JSON.parse(localStorage.getItem('downloads'))?.downloads || 200000
+    downloads: JSON.parse(getLocalStorage('downloads'))?.downloads || 200000
 });
 
 export default {
@@ -153,7 +153,7 @@ async function getDownloadValue(url, attempts = 3, delay = 500) {
 
 async function getDownloads() {
     const now = Date.now();
-    const cachedDownloads = JSON.parse(localStorage.getItem('downloads'));
+    const cachedDownloads = JSON.parse(getLocalStorage('downloads'));
     if (cachedDownloads?.timestamp && (now - cachedDownloads.timestamp < 60 * 60_000)) return cachedDownloads.downloads;
     let total = 0;
     let cache = true;
@@ -173,13 +173,13 @@ async function getDownloads() {
         }
     }
     state.downloads = cache ? total : (cachedDownloads?.downloads || total);
-    if (cache) localStorage.setItem('downloads', JSON.stringify({ downloads: total, timestamp: now }));
+    if (cache) setLocalStorage('downloads', JSON.stringify({ downloads: total, timestamp: now }));
     return total;
 }
 
 async function getMembers() {
     const now = Date.now();
-    const cachedMembers = JSON.parse(localStorage.getItem('discordMembers'));
+    const cachedMembers = JSON.parse(getLocalStorage('discordMembers'));
     if (cachedMembers?.timestamp && (now - cachedMembers.timestamp < 5 * 60_000)) return;
     let members = 0;
     let cache = true;
@@ -191,12 +191,12 @@ async function getMembers() {
     }
 
     state.discord.members = members;
-    if (cache) localStorage.setItem('discordMembers', JSON.stringify({ members, timestamp: now }));
+    if (cache) setLocalStorage('discordMembers', JSON.stringify({ members, timestamp: now }));
 }
 
 async function getStars() {
     const now = Date.now();
-    const cachedStats = JSON.parse(localStorage.getItem('gitStats'));
+    const cachedStats = JSON.parse(getLocalStorage('gitStats'));
     if (cachedStats?.timestamp && (now - cachedStats.timestamp < 360 * 60_000)) return;
     let stars = 0;
     let forks = 0;
@@ -214,7 +214,7 @@ async function getStars() {
 
     state.github.stars = cache ? stars : (cachedStats?.stars || stars);
     state.github.forks = cache ? forks : (cachedStats?.forks || forks);
-    if (cache) localStorage.setItem('gitStats', JSON.stringify({ stars, forks, timestamp: now }));
+    if (cache) setLocalStorage('gitStats', JSON.stringify({ stars, forks, timestamp: now }));
 }
 
 function getVersionFromArtifact(name, fileName) {
@@ -259,7 +259,7 @@ async function getCommits(currentCI, name, buildNumber, lastResponse) {
             break;
         }
         const currentBuild = buildNumber;
-        const cachedCommits = JSON.parse(localStorage.getItem(`commits${currentCI}${currentBuild}`));
+        const cachedCommits = JSON.parse(getLocalStorage(`commits${currentCI}${currentBuild}`));
         try {
             let response;
             let cache = true;
@@ -285,7 +285,7 @@ async function getCommits(currentCI, name, buildNumber, lastResponse) {
                 });
             }
             buildNumber = response.data.id - 1;
-            if (cache) localStorage.setItem(`commits${currentCI}${currentBuild}`, JSON.stringify({ response, timestamp: now }));
+            if (cache) setLocalStorage(`commits${currentCI}${currentBuild}`, JSON.stringify({ response, timestamp: now }));
         } catch (e) {
             if (e.response && e.response.status === 404) {
                 buildNumber -= 1;
@@ -311,7 +311,7 @@ async function getJenkins() {
             state.builds.dev.plugins[name].lastCommits = [];
             state.builds.stable.plugins[name].commits = [];
 
-            const cachedJenkins = JSON.parse(localStorage.getItem(`jenkins${pluginsDefaults[name].name}`));
+            const cachedJenkins = JSON.parse(getLocalStorage(`jenkins${pluginsDefaults[name].name}`));
             let response;
             let cache = true;
             if (cachedJenkins?.timestamp && (now - cachedJenkins.timestamp < 60_000)) {
@@ -325,7 +325,7 @@ async function getJenkins() {
             getCommits(currentCI, name, (response.data.id - 1), response);
             state.builds.dev.error = null;
             state.builds.stable.error = null;
-            if (cache) localStorage.setItem(`jenkins${pluginsDefaults[name].name}`, JSON.stringify({ response, timestamp: now }));
+            if (cache) setLocalStorage(`jenkins${pluginsDefaults[name].name}`, JSON.stringify({ response, timestamp: now }));
         }
     } catch (e) {
         state.builds.dev.error = e.response ? e.response.data : e.message;
@@ -350,7 +350,7 @@ async function getLatestRelease() {
         state.builds.stable.loading = true;
         const now = Date.now();
         for (const name in pluginsDefaults) {
-            const cachedReleases = JSON.parse(localStorage.getItem(`releases${pluginsDefaults[name].name}`));
+            const cachedReleases = JSON.parse(getLocalStorage(`releases${pluginsDefaults[name].name}`));
             let data;
             let cache = true;
             if (cachedReleases?.timestamp && (now - cachedReleases.timestamp < 5 * 60_000)) {
@@ -362,7 +362,7 @@ async function getLatestRelease() {
             state.builds.stable.plugins[name].timestamp = String(data[0].created_at);
             state.builds.stable.error = null;
             data[0].assets.forEach(asset => state.builds.stable.plugins[name].downloadUrl = asset.browser_download_url);
-            if (cache) localStorage.setItem(`releases${pluginsDefaults[name].name}`, JSON.stringify({ data, timestamp: now }));
+            if (cache) setLocalStorage(`releases${pluginsDefaults[name].name}`, JSON.stringify({ data, timestamp: now }));
         }
     } catch (e) {
         state.builds.stable.error = e.response ? e.response.data : e.message;
@@ -373,7 +373,7 @@ async function getLatestRelease() {
 async function getDescription() {
     const now = Date.now();
     for (const name in Object.fromEntries(Object.entries(pluginsDefaults).filter(([_, plugin]) => !plugin.gitOnly))) {
-        const cachedDescription = JSON.parse(localStorage.getItem(`description${pluginsDefaults[name].name}`))
+        const cachedDescription = JSON.parse(getLocalStorage(`description${pluginsDefaults[name].name}`))
         if (cachedDescription?.timestamp && (now - cachedDescription.timestamp < 5 * 60_000)) {
             state.builds.dev.plugins[name].description = cachedDescription.devDescription;
             state.builds.stable.plugins[name].description = cachedDescription.releaseDescription;
@@ -390,7 +390,18 @@ async function getDescription() {
         let releaseParts = description.split('<ul class="snapshot-versions"')
         const releaseDescription = releaseParts[0] + '<ul class="release-versions"' + releaseParts[1].split('<ul class="release-versions"')[1];
         state.builds.stable.plugins[name].description = releaseDescription;
-        localStorage.setItem(`description${pluginsDefaults[name].name}`, JSON.stringify({ devDescription, releaseDescription, timestamp: now }));
+        setLocalStorage(`description${pluginsDefaults[name].name}`, JSON.stringify({ devDescription, releaseDescription, timestamp: now }));
+    }
+}
+
+function getLocalStorage(key) {
+    if (typeof window !== 'undefined' && window.localStorage) return localStorage.getItem(key);
+    return null;
+}
+
+function setLocalStorage(key, value) {
+    if (typeof window !== 'undefined' && window.localStorage) {
+        localStorage.setItem(key, value);
     }
 }
 
